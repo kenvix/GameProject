a32cb2ace2b94093717f43b8b3a35e48a8c46eac
Kenvix <kenvixzure@live.com>
2018-12-17 20:32:22 +0800

    fix map again

--- GameProject/screen/page/map.h
+++ GameProject/screen/page/map.h
@@ -16,7 +16,7 @@ inline int show_map() {
 	cleanup();
 	put_background("map.jpg");
 	draw_player_info();
-	int conse;
+	int conse = 0;
 	while (true) {
 		const MOUSEMSG Mouse = GetMouseMsg();
 		if ((Mouse.x >= 939 && Mouse.x <= 1011) && (Mouse.y >= 547 && Mouse.y <= 600)) {
@@ -25,31 +25,31 @@ inline int show_map() {
 				break;
 			}
 		} else {
-			if ((Mouse.x >= 488 && Mouse.x <= 552) && (Mouse.y >= 100 && Mousew.y <= 160)) {
+			if ((Mouse.x >= 488 && Mouse.x <= 552) && (Mouse.y >= 100 && Mouse.y <= 160)) {
 				if (Mouse.mkLButton) {
 					conse = 1;
 					break;
 				}
 			} else {
-				if ((Mouse.x >= 676 && Mouse.x <= 738) && (Mouse.y >= 243 && Mousew.y <= 299)) {
+				if ((Mouse.x >= 676 && Mouse.x <= 738) && (Mouse.y >= 243 && Mouse.y <= 299)) {
 					if (Mouse.mkLButton) {
 						conse = 2;
 						break;
 					}
 				} else {
-					if ((Mouse.x >= 600 && Mouse.x <= 661) && (Mouse.y >= 424 && Mousew.y <= 481)) {
+					if ((Mouse.x >= 600 && Mouse.x <= 661) && (Mouse.y >= 424 && Mouse.y <= 481)) {
 						if (Mouse.mkLButton) {
 							conse = 3;
 							break;
 						}
 					} else {
-						if ((Mouse.x >= 382 && Mouse.x <= 442) && (Mouse.y >= 424 && Mousew.y <= 481)) {
+						if ((Mouse.x >= 382 && Mouse.x <= 442) && (Mouse.y >= 424 && Mouse.y <= 481)) {
 							if (Mouse.mkLButton) {
 								conse = 4;
 								break;
 							}
 						} else {
-							if ((Mouse.x >= 307 && Mouse.x <= 371) && (Mouse.y >= 243 && Mousew.y <= 299)) {
+							if ((Mouse.x >= 307 && Mouse.x <= 371) && (Mouse.y >= 243 && Mouse.y <= 299)) {
 								if (Mouse.mkLButton) {
 									conse = 5;
 									break;
