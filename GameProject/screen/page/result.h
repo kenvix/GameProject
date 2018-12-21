ea5c051c73345ee2c7d8fc82d7f7fe7a5acb1fca
KingDong <W.H.dong943@outlook.com>
2018-12-21 21:41:20 +0800

    否归

--- GameProject/screen/page/result.h
+++ GameProject/screen/page/result.h
@@ -7,50 +7,52 @@
 
 extern GamePlayer* player;
 
-inline void show_result(GameRound* round) {
+inline int show_result(GameRound* round) {
 	int no;
 	// 绘制游戏结束界面
-	POINT pts[] = { {5,5},{611,5},{611,305},{5,305} };
+	POINT pts[] = { {193,129},{802,129},{802,433},{193,433} };
 	polygon(pts, 4);
-	POINT pot[] = { {59,239},{59,270},{181,270},{181,239} };
+	setfillcolor(BLACK);
+	fillrectangle(193,129,802,433);
+	POINT pot[] = { {221,365},{413,365},{413,406},{221,406} };
 	polygon(pot, 4);
-	POINT pet[] = { {450,239},{450,270},{573,270},{573,239} };
+	POINT pet[] = { {576,365},{770,365},{770,406},{576,406} };
 	polygon(pet, 4);
-	circle(450, 100, 70);
+	circle(670, 227, 70);
 	char* strbuffer = (char*) calloc(133, sizeof(char));
 
-	RECT rect = { 65,35, WINDOW_WIDTH, WINDOW_HEIGHT };
+	RECT rect = { 250,175, WINDOW_WIDTH, WINDOW_HEIGHT };
 	sprintf_s(strbuffer, 133, "Total Score: %15u", round->score);
 	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
 
-	rect = { 65,70, WINDOW_WIDTH, WINDOW_HEIGHT };
+	rect = { 250,210, WINDOW_WIDTH, WINDOW_HEIGHT };
 	sprintf_s(strbuffer, 133, "Max Combo: %17u", round->max_combo);
 	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
 
-	rect = { 65,105, WINDOW_WIDTH, WINDOW_HEIGHT };
+	rect = { 250,245, WINDOW_WIDTH, WINDOW_HEIGHT };
 	sprintf_s(strbuffer, 133, "Level: %21u", round->max_combo);
 	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
 
-	rect = { 65,140, WINDOW_WIDTH, WINDOW_HEIGHT };
+	rect = { 250,280, WINDOW_WIDTH, WINDOW_HEIGHT };
 	sprintf_s(strbuffer, 133, "Experience: %16u", round->max_combo);;
 	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
 
-	rect = { 80,248, WINDOW_WIDTH, WINDOW_HEIGHT };
+	rect = { 285,377, WINDOW_WIDTH, WINDOW_HEIGHT };
 	drawtext(_T("RECORD"), &rect, DT_SINGLELINE);
 
-	rect = { 473,248, WINDOW_WIDTH, WINDOW_HEIGHT };
+	rect = { 640,377, WINDOW_WIDTH, WINDOW_HEIGHT };
 	drawtext(_T("CONTINUE"), &rect, DT_SINGLELINE);
 	// 捕捉鼠标信息
 	while (true) {
 		const MOUSEMSG Mouse = GetMouseMsg();
-		if ((Mouse.x >= 59 && Mouse.x <= 181) && (Mouse.y >= 239 && Mouse.y <= 270)) {
+		if ((Mouse.x >= 221 && Mouse.x <= 413) && (Mouse.y >= 365 && Mouse.y <= 406)) {
 			if (Mouse.mkLButton) {
 				no = 1;   //判断是否点击游戏记录 (RECORD)
 				break;
 			}
 		}
 		else {
-			if ((Mouse.x >= 450 && Mouse.x <= 573) && (Mouse.y >= 239 && Mouse.y <= 270)) {
+			if ((Mouse.x >= 576 && Mouse.x <= 770) && (Mouse.y >= 365 && Mouse.y <= 406)) {
 				if (Mouse.mkLButton) {
 					no = 2;   //判断是否点击 继续游戏 (CONTINUE)
 					break;
@@ -61,5 +63,6 @@ inline void show_result(GameRound* round) {
 	FlushMouseMsgBuffer();
 	_getch();
 	closegraph();
+	return no;
 }
 #endif
