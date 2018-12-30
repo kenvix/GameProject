e73151dcae33ad71a699d25921567fc69a53053c
KingDong <W.H.dong943@outlook.com>
2018-12-30 13:09:23 +0800

    反倒是

--- GameProject/screen/page/record.h
+++ GameProject/screen/page/record.h
@@ -40,70 +40,76 @@ inline void show_record() {
 		GameMap* mapinfo = get_map_info(current_round.map);
 		rect = { 183,124,WINDOW_WIDTH,WINDOW_HEIGHT };
 		drawtext(_T(mapinfo->name), &rect, DT_SINGLELINE);
-		rect = { 286,124,WINDOW_WIDTH,WINDOW_HEIGHT };
+		rect = { 326,124,WINDOW_WIDTH,WINDOW_HEIGHT };
 		drawtext(_T(mapinfo->description), &rect, DT_SINGLELINE);
-		rect = { 440,124,WINDOW_WIDTH,WINDOW_HEIGHT };
+		rect = { 430,124,WINDOW_WIDTH,WINDOW_HEIGHT };
 		sprintf_s(strbuffer, 133, "%6u", current_round.max_combo);
 		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-		rect = { 610,124,WINDOW_WIDTH,WINDOW_HEIGHT };
+		rect = { 540,124,WINDOW_WIDTH,WINDOW_HEIGHT };
 		sprintf_s(strbuffer, 133, "%6u", current_round.score);
 		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-		rect = { 753,124,WINDOW_WIDTH,WINDOW_HEIGHT };
+		rect = { 653,124,WINDOW_WIDTH,WINDOW_HEIGHT };
 		localtime_s(tm, &current_round.time);
 		strftime(strbuffer, 133, "%Y-%m-%d %H:%M:%S", tm);
 		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
 	}
-	//
-	//if(rounds_num >= 2) {
-	//	current_round = &rounds_basic[rounds_num - 2];
-	//	rect = { 183,229,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	drawtext(_T("SECOND"), &rect, DT_SINGLELINE);
-	//	rect = { 286,229,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	drawtext(_T(current_round->map), &rect, DT_SINGLELINE);
-	//	rect = { 440,229,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->max_combo);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//	rect = { 610,229,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->score);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//	rect = { 753,229,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->time);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//}
+	
+	if(rounds_num >= 2) {
+		GameRound current_round = rounds_basic[rounds_num - 2];
+		GameMap* mapinfo = get_map_info(current_round.map);
+		rect = { 183,229,WINDOW_WIDTH,WINDOW_HEIGHT };
+		drawtext(_T(mapinfo->name), &rect, DT_SINGLELINE);
+		rect = { 326,124,WINDOW_WIDTH,WINDOW_HEIGHT };
+		drawtext(_T(mapinfo->description), &rect, DT_SINGLELINE);
+		rect = { 430,229,WINDOW_WIDTH,WINDOW_HEIGHT };
+		sprintf_s(strbuffer, 133, "%6u", current_round.max_combo);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+		rect = { 540,229,WINDOW_WIDTH,WINDOW_HEIGHT };
+		sprintf_s(strbuffer, 133, "%6u", current_round.score);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+		rect = { 653,229,WINDOW_WIDTH,WINDOW_HEIGHT };
+		localtime_s(tm, &current_round.time);
+		strftime(strbuffer, 133, "%Y-%m-%d %H:%M:%S", tm);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+	}
 
-	//if(rounds_num >= 3) {
-	//	current_round = &rounds_basic[rounds_num - 3];
-	//	rect = { 183,341,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	drawtext(_T("THIRD"), &rect, DT_SINGLELINE);
-	//	rect = { 286,341,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	drawtext(_T(current_round->map), &rect, DT_SINGLELINE);
-	//	rect = { 440,341,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->max_combo);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//	rect = { 610,341,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->score);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//	rect = { 753,341,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->time);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//}
+	if(rounds_num >= 3) {
+		GameRound current_round = rounds_basic[rounds_num - 1];
+		GameMap* mapinfo = get_map_info(current_round.map);
+		rect = { 183,341,WINDOW_WIDTH,WINDOW_HEIGHT };
+		drawtext(_T(mapinfo->name), &rect, DT_SINGLELINE);
+		rect = { 326 ,341,WINDOW_WIDTH,WINDOW_HEIGHT };
+		drawtext(_T(mapinfo->description), &rect, DT_SINGLELINE);
+		rect = { 395,341,WINDOW_WIDTH,WINDOW_HEIGHT };
+		sprintf_s(strbuffer, 133, "%6u", current_round.max_combo);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+		rect = { 500,341,WINDOW_WIDTH,WINDOW_HEIGHT };
+		sprintf_s(strbuffer, 133, "%6u", current_round.score);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+		rect = { 653,341,WINDOW_WIDTH,WINDOW_HEIGHT };
+		localtime_s(tm, &current_round.time);
+		strftime(strbuffer, 133, "%Y-%m-%d %H:%M:%S", tm);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+	}
 
-	//if(rounds_num >= 4) {
-	//	current_round = &rounds_basic[rounds_num - 4];
-	//	rect = { 183,448,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	drawtext(_T("FORTH"), &rect, DT_SINGLELINE);
-	//	rect = { 286,448,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	drawtext(_T(current_round->map), &rect, DT_SINGLELINE);
-	//	rect = { 440,448,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->max_combo);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//	rect = { 610,448,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->score);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//	rect = { 753,448,WINDOW_WIDTH,WINDOW_HEIGHT };
-	//	sprintf_s(strbuffer, 133, "%6u", current_round->time);
-	//	drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
-	//}
+	if(rounds_num >= 4) {
+		GameRound current_round = rounds_basic[rounds_num - 1];
+		GameMap* mapinfo = get_map_info(current_round.map);
+		rect = { 183,448,WINDOW_WIDTH,WINDOW_HEIGHT };
+		drawtext(_T(mapinfo->name), &rect, DT_SINGLELINE);
+		rect = { 286,448,WINDOW_WIDTH,WINDOW_HEIGHT };
+		drawtext(_T(mapinfo->description), &rect, DT_SINGLELINE);
+		rect = { 395 ,448,WINDOW_WIDTH,WINDOW_HEIGHT };
+		sprintf_s(strbuffer, 133, "%6u", current_round.max_combo);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+		rect = { 500,448,WINDOW_WIDTH,WINDOW_HEIGHT };
+		sprintf_s(strbuffer, 133, "%6u", current_round.score);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+		rect = { 653,448,WINDOW_WIDTH,WINDOW_HEIGHT };
+		localtime_s(tm, &current_round.time);
+		strftime(strbuffer, 133, "%Y-%m-%d %H:%M:%S", tm);
+		drawtext(_T(strbuffer), &rect, DT_SINGLELINE);
+	}
 
 	while (true) {
 		const MOUSEMSG Mouse = GetMouseMsg();
